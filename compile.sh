32f854bab1d0d32a393f10a9dff2e9bb8da77795
grimir <falta.vit@zsbcuo.cz>
2021-03-02 20:08:38 +0100

    init

---
+++ compile.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+
+rm -rf build
+
+cp -rf src build
+
+cd build
+
+ls build
+
+echo Running vlna to correct czech spelling
+
+for filename in *.tex; do
+    iconv -f UTF-8 -t ISO-8859-2 "$filename" -o "$filename"
+    vlna -l -m -n  "$filename"
+    iconv -f ISO-8859-2 -t UTF-8 "$filename" -o "$filename"
+  done
+
+rm *~
+
+
+latexmk -pdf -f -bibtex -interaction=nonstopmode 
+
+mv *.pdf ../out.pdf
