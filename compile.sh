8a91a3a722c51589294a0e3029c0d415737930d6
grimir <falta.vit@zsbcuo.cz>
2021-03-02 21:07:13 +0100

    added více-serverové nasazení

--- compile.sh
+++ compile.sh
@@ -8,14 +8,18 @@ cd build
 
 echo Running vlna to correct czech spelling
 
+touch all.log
+
 for filename in *.tex; do
-    iconv -f UTF-8 -t ISO-8859-2 "$filename" -o "$filename"
-    vlna -l -m -n  "$filename"
-    iconv -f ISO-8859-2 -t UTF-8 "$filename" -o "$filename"
+    iconv -f UTF-8 -t ISO-8859-2 "$filename" -o "$filename" >> all.log 2>&1
+    vlna -l -m -n  "$filename" >> all.log 2>&1
+    iconv -f ISO-8859-2 -t UTF-8 "$filename" -o "$filename" >> all.log 2>&1
   done
 
 rm *~
 
-latexmk -pdf -f -bibtex -interaction=nonstopmode 
+echo Compiling latex to pdf
+
+time latexmk -pdf -f -bibtex -interaction=nonstopmode >> all.log 2>&1
 
 mv *.pdf ../out.pdf
