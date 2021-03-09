fcf894abf1d9fa98dca5814214510b2cb34d5f45
Vít Falta <falta.vit@zsbcuo.cz>
2021-03-09 21:26:44 +0100

    9.3.2021

--- compile.sh
+++ compile.sh
@@ -11,15 +11,17 @@ echo Running vlna to correct czech spelling
 touch all.log
 
 for filename in *.tex; do
-    iconv -f UTF-8 -t ISO-8859-2 "$filename" -o "$filename" >> all.log 2>&1
+    #iconv -f UTF-8 -t ISO-8859-2 "$filename" -o "$filename" >> all.log 2>&1
     vlna -l -m -n  "$filename" >> all.log 2>&1
-    iconv -f ISO-8859-2 -t UTF-8 "$filename" -o "$filename" >> all.log 2>&1
+    #iconv -f ISO-8859-2 -t UTF-8 "$filename" -o "$filename" >> all.log 2>&1
+    # Remove BOM
+    sed -i '' -e "s/\xEF\xBB\xBF//" "$filename"
   done
 
 rm *~
 
 echo Compiling latex to pdf
 
-time latexmk -pdf -f -bibtex -interaction=nonstopmode >> all.log 2>&1
+time latexmk -pdf -pdflatex -f -bibtex -interaction=nonstopmode >> all.log 2>&1
 
 mv *.pdf ../out.pdf
